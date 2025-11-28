0f2633c2e57a6284e4b0d6684bfbc861654d8597
FenrisuIven <mostova.alona.m@gmail.com>
2025-11-28 10:38:07 +0200

    feat: basic bottom content row

--- LB1/app/src/main/java/com/example/lb1/ui/screens/notifications/NotificationsScreen.kt
+++ LB1/app/src/main/java/com/example/lb1/ui/screens/notifications/NotificationsScreen.kt
@@ -1,16 +1,24 @@
 package com.example.lb1.ui.screens.notifications
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.lb1.model.Notification
@@ -43,6 +51,25 @@ fun NotificationsScreen(
           removeAction = { viewModel.removeNotification(listItem.id) },
           clickAction = { navController.navigate("details?notifId=${listItem.id}") }
         )
+      }
+    }
+    LazyRow(
+      horizontalArrangement = Arrangement.spacedBy(8.dp),
+      modifier = Modifier
+        .padding(8.dp, 0.dp)
+    ) {
+      itemsIndexed(notificationsList.value!!) { _: Int, listItem: Notification ->
+        Box(
+          modifier = Modifier
+            .background(Color.LightGray)
+            .height(64.dp)
+            .width(64.dp)
+        ) {
+          Text(
+            text = "${listItem.title.substring(0, 3)}...",
+            modifier = Modifier.align(Alignment.Center)
+          )
+        }
       }
     }
   }
